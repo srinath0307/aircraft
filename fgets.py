72acd0b7d7c90854df6d637247faf7c290f9d67a
srinath0307 <srinathmurali2003@gmail.com>
2023-03-06 18:58:49 +0530

    updates

--- fgets.py
+++
@@ -1,4 +0,0 @@
-num1=10000000000000
-num2=1000000000
-total=num1+num2
-print(f'{total:,}')
--- infinite_loop2.py
+++
@@ -1,9 +0,0 @@
-x=1
-while x>0:
-    print("python:")
-    print("invented by guido van rossum")
-    print("this is an infinite loop")
-    print("created by srinath")
-    print("something in the way")
-    x=x+1
-    
