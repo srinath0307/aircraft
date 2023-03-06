e29de0497ab0a0c260738e44742145de7da46b25
srinath murali <in_srinath@vertace.com>
2023-03-06 16:28:05 +0530

    adding files

--- areaSorted.py
+++ areaSorted.py
@@ -1,23 +1,21 @@
-'''n=int(input())
-pair=[]
+n = int(input())
+pair = []
 for i in range(n):
-    l,b=map(int,input().split())
-    pair.append([l,b])
+    l, b = map(int, input().split())
+    pair.append([l, b])
 print(pair)
-area=[]
-for i,j in pair:
-    value=i*j
+area = []
+for i, j in pair:
+    value = i * j
     area.append(value)
 print(area)
-liist=[]
-x=0
-while(x<len(pair)-1):
-    for i,j in pair:
-        r=[i,j,area[x]]
+liist = []
+x = 0
+while (x < len(pair) - 1):
+    for i, j in pair:
+        r = [i, j, area[x]]
         liist.append(r)
-        x+=1
+        x += 1
 print(liist)
-so=sorted(liist,key=lambda x:(x[2],x[1]),reverse=True)
+so = sorted(liist, key=lambda x: (x[2], x[1]), reverse=True)
 print(so)
-'''
-
